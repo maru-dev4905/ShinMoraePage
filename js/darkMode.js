09b231569970c8f4f02bf380a988f45043b8789a
Seungyeol <marudev4905@gmail.com>
2020-03-27 15:09:54 +0900

    gallery 2018 추가

--- js/darkMode.js
+++ js/darkMode.js
@@ -2,11 +2,11 @@ const changeBtn = document.querySelector(".aside--btn");
 const aside = document.querySelector("aside");
 const aGroup = document.querySelectorAll("a");
 const banner = document.querySelector(".banner");
-const mainSection = document.querySelector(".section--main");
-const mainSectionText = document.querySelector(".section--main--text");
+
 const mainImgCouple = document.querySelector(".main__img_couple");
 const mainImgNight = document.querySelector(".main__img_night");
 const bannerText = banner.childNodes;
+const mainSection = document.querySelector(".section--main");
 
 const CHANGECHECK = "darkMode";
 const LOCALCHECK = localStorage.getItem(CHANGECHECK);
@@ -14,7 +14,10 @@ const LOCALCHECK = localStorage.getItem(CHANGECHECK);
 let darkModeCheck = "white";
 let path = window.location.pathname;
 path = path.split("/").pop();
- 
+
+if(path === "index.html"){
+    const mainSectionText = document.querySelector(".section--main--text");
+}
 blackMode = () => {
     aside.style.backgroundColor = "#E2E2E2"
     
@@ -23,10 +26,10 @@ blackMode = () => {
     }
     banner.style.backgroundColor = "#fff";
     bannerText[1].style.color = "#000";
+    
     mainSection.style.backgroundColor = "#f4f4f4";
-    mainSectionText.style.color = "#000";
-
     if(path==="index.html"){
+        mainSectionText.style.color = "#000";
         mainImgCouple.setAttribute("src","./image/artwork/2016/th-12_nov_2016_2.jpg");
         mainImgNight.setAttribute("src","./image/artwork/2015/th-11_yandb2.jpg");
     }
@@ -36,12 +39,12 @@ whiteMode = () => {
 
     for(let i=0; i < aGroup.length; i++){
         aGroup[i].style.color="#fff";
+        banner.style.backgroundColor = "#000";
     }
-    banner.style.backgroundColor = "#000";
     bannerText[1].style.color = "#fff";
-    mainSection.style.backgroundColor = "#4b4b4b";
-    mainSectionText.style.color = "#fff";
     if(path==="index.html"){
+        mainSection.style.backgroundColor = "#4b4b4b";
+        mainSectionText.style.color = "#fff";
         mainImgCouple.setAttribute("src","./image/artwork/2016/th-12_nov_2016.jpg");
         mainImgNight.setAttribute("src","./image/artwork/2015/th-11_yandb3.png");
     }
